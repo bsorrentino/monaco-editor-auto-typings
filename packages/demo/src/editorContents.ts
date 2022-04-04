0a2ebbfda28e009646549f4c240279754f9db63a
bsorrentino <bartolomeo.sorrentino@gmail.com>
2022-04-04 18:52:34 +0200

    add simple sample with scoped package

--- packages/demo/src/editorContents.ts
+++ packages/demo/src/editorContents.ts
@@ -1,15 +1,22 @@
-export const editorContents = `import fetch from 'node-fetch'
-import React from 'react'
-import { LocalStorageCache } from 'monaco-editor-auto-typings'
+// export const editorContents = `import fetch from 'node-fetch'
+// import React from 'react'
+// import { LocalStorageCache } from 'monaco-editor-auto-typings'
 
-// Works fine
-fetch('https://google.com', { method: 'GET' })
-React.useEffect(() => {})
-React.useState<string>('Hello')
-new LocalStorageCache().getFile('FILE_ID')
+// // Works fine
+// fetch('https://google.com', { method: 'GET' })
+// React.useEffect(() => {})
+// React.useState<string>('Hello')
+// new LocalStorageCache().getFile('FILE_ID')
 
-// Type errors are detected! :)
-fetch(1337) // Shouldn't be a number!
-React.useEffect('I\`m not a function!')
-React.useState<number>('Not a number :s')
-new LocalStorageCache().getFile() // Argument missing!`;
+// // Type errors are detected! :)
+// fetch(1337) // Shouldn't be a number!
+// React.useEffect('I\`m not a function!')
+// React.useState<number>('Not a number :s')
+// new LocalStorageCache().getFile() // Argument missing!`;
+
+export const editorContents = `
+import { uuidV4 } from "@daz.is/uuid-v4";
+import React from 'react';
+
+const uuid = uuidV4();
+`
