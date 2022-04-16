0739d95c0994b1d11d7fdf190878cb2d8c335772
Build Pipeline <build.pipeline@softphone.it>
2022-04-16 23:45:18 +0200

    demo refinements

--- packages/demo/src/editorContents.ts
+++ packages/demo/src/editorContents.ts
@@ -23,6 +23,18 @@
 // `
 
 export const editorContents = `
-import type from "@types/xrm"
-import { uuidV4 } from "@daz.is/uuid-v4";
+/// <reference types="@types/xrm" />
+
+import {html, render} from 'lit-html';
+
+let c:Partial<Xrm.Metadata.EntityMetadata> = {}
+
+// This is a lit-html template function. It returns a lit-html template.
+const helloTemplate = (name) => html\`<div>Hello ${name}!</div>\`;
+
+// This renders <div>Hello Steve!</div> to the document body
+render(helloTemplate('Steve'), document.body);
+
+// This updates to <div>Hello Kevin!</div>, but only updates the ${name} part
+render(helloTemplate('Kevin'), document.body);
 `
--- packages/demo/src/index.ts
+++ packages/demo/src/index.ts
@@ -4,9 +4,35 @@ import { editorContents } from './editorContents';
 
 import './style.css';
 
+
+// monaco.languages.typescript.typescriptDefaults.setCompilerOptions({
+
+//   importsNotUsedAsValues: 'preserve'
+// })
+
+monaco.languages.typescript.typescriptDefaults.setCompilerOptions({
+  target: monaco.languages.typescript.ScriptTarget.ES2016,
+  moduleResolution: monaco.languages.typescript.ModuleResolutionKind.NodeJs,
+  module: monaco.languages.typescript.ModuleKind.CommonJS,
+  allowNonTsExtensions: true,
+  noEmit: true,
+  // esModuleInterop: true,
+  // allowJs: true,      
+  jsx: monaco.languages.typescript.JsxEmit.React,
+  // typeRoots: [TYPES_ROOT]
+})
+// monaco.languages.typescript.typescriptDefaults.setEagerModelSync(true);
+
+monaco.languages.typescript.typescriptDefaults.setDiagnosticsOptions({
+  noSemanticValidation: false,
+  noSyntaxValidation: false,
+  noSuggestionDiagnostics: true
+})
+
 const editor = monaco.editor.create(document.getElementById('editor-mountpoint')!, {
   model: monaco.editor.createModel(editorContents, 'typescript' /*Uri.parse('file://root/index.ts')*/),
 });
+
 
 const cache = new LocalStorageCache();
 
