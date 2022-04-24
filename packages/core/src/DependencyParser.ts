ce0e6070736461470467e4f8d6b532481915983e
bsorrentino <bartolomeo.sorrentino@gmail.com>
2022-04-24 17:21:20 +0200

    return to original implementation

--- packages/core/src/DependencyParser.ts
+++ packages/core/src/DependencyParser.ts
@@ -1,18 +1,6 @@
 import { ImportResourcePath } from './ImportResourcePath';
 import * as path from 'path';
 
-
-const importPath_ext_to_remove = ['.js', '.mjs']
-
-const shouldBeImportPathNormalized = ( importPath: string  ) => {
-  const ext = path.extname( importPath )
-  return ( ext.length >= 3 ) ?
-    importPath_ext_to_remove.find( (v) => v === ext ) : undefined
-}
-
-const normalizeImportPath = ( importPath: string, ext: string  ) => importPath.slice(0, -ext.length )
-
-
 export class DependencyParser {
   private REGEX_CLEAN = /[\n|\r]/g;
   private REGEX_DETECT_IMPORT = /(?:(?:import|export)(?:.)*?from\s+["']([^"']+)["'])|(?:\/+\s+<reference\s+(?:path|types)=["']([^"']+)["']\s+\/>)/g;
@@ -33,37 +21,6 @@ export class DependencyParser {
   }
 
   private resolvePath(importPath: string, isTypeOnly:boolean, parent: ImportResourcePath | string): ImportResourcePath {
-    
-    const result = this._resolvePath( importPath, isTypeOnly, parent )
-    
-    console.groupCollapsed(`resolvePath: ${importPath}`)
-    console.group( 'input' )
-    console.log( 'importPath', importPath )
-    console.log( 'isTypeOnly', isTypeOnly )
-    console.log( 'parent', parent )
-    console.groupEnd()
-
-    if( result.importPath ) {
-      const ext = shouldBeImportPathNormalized( result.importPath )
-      if( ext ) {
-        switch( result.kind ) {
-          case 'package': 
-          case 'relative':
-            result.importPath = normalizeImportPath( result.importPath, ext )
-            break
-          case 'relative-in-package':
-            // result.importPath = normalizeImportPath( result.importPath, ext )
-            const importPath = normalizeImportPath( result.importPath, ext )
-            // result.packageName = path.basename(importPath)
-            result.importPath = importPath
-        }
-      }
-    }
-
-    return result
-  }
-
-  private _resolvePath(importPath: string, isTypeOnly:boolean, parent: ImportResourcePath | string): ImportResourcePath {
     if (typeof parent === 'string') {
       if (importPath.startsWith('.')) {
         return {
