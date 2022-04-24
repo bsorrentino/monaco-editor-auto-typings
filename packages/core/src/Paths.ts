a7eeb137f0b21e18baa977b1601e9c2ae2d4637e
bsorrentino <bartolomeo.sorrentino@gmail.com>
2022-04-24 22:08:48 +0200

    remove console logging

--- packages/core/src/Paths.ts
+++ packages/core/src/Paths.ts
@@ -1,54 +1,14 @@
 
 
-export const pathEndsWithSourceExt = (thePath: string) => {
-    console.group(`pathEndsWithSourceExt: ${thePath}`)
+export const pathEndsWithSourceExt = (thePath: string) => 
+    thePath.match( /(.js|.mjs)$/ig )!==null
 
-    const result =  thePath.match( /(.js|.mjs)$/ig )!==null
+export const pathReplaceSourceExtAtEnd = ( thePath: string, replace = '' ) => 
+    thePath.replace( /(.js|.mjs)$/ig, replace )
 
-    console.log( 'result', result )
-    console.groupEnd()
+export const pathContainsSourceExt = (thePath: string) => 
+    thePath.match( /((?:.js|.mjs).*)/ig )!==null
 
-    return result
-
-}
-
-export const pathReplaceSourceExtAtEnd = ( thePath: string, replace = '' ) => {
-    console.group(`pathReplaceSourceExtAtEnd: ${thePath}`)
-
-    const result = thePath.replace( /(.js|.mjs)$/ig, replace )
-    
-    console.log( 'result', result )
-
-    console.groupEnd()
-
-    return result
-
-}
-
-
-export const pathContainsSourceExt = (thePath: string) => {
-    console.group(`pathContainsSourceExt: ${thePath}`)
-
-    const result =  thePath.match( /((?:.js|.mjs).*)/ig )!==null
-
-    console.log( 'result', result )
-    console.groupEnd()
-
-    return result
-
-}
-
-export const pathReplaceSourceExt = ( thePath: string, replace = '' ) => {
-    console.group(`pathReplaceSourceExt: ${thePath}`)
-
-    const result = thePath.replace( /((?:.js|.mjs).*)/ig, replace  )
-    //const result = thePath.replace( /(.js|.mjs)/ig, '' )
-
-    console.log( 'result', result )
-    console.groupEnd()
-
-    return result
-
-}
-  
+export const pathReplaceSourceExt = ( thePath: string, replace = '' ) => 
+    thePath.replace( /((?:.js|.mjs).*)/ig, replace  )
   
